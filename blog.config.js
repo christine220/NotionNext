192d5308517886beb6ea2ad7a0c505374f7a8515
christine220 <162721326+christine220@users.noreply.github.com>
2024-03-09 20:24:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,8 +19,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '刘圈圈', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '普普通通一只羊🐑', // 作者简介
-  //LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'christinejourney.com', // 网站地址
+  // KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'js.dove523@gmail.com', // 邮箱地址 例如mail@tangly1024.com
